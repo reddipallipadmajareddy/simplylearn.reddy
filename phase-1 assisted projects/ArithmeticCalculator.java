a4f65710f67d45e68a2ba14bd4b49ff01c98594b
reddipallipadmajareddy <97421398+reddipallipadmajareddy@users.noreply.github.com>
2022-01-26 09:41:07 +0530

    Add files via upload
    Initial Commit

---
+++ phase-1 assisted projects/ArithmeticCalculator.java
@@ -0,0 +1,28 @@
+package com.padmaja.project;
+import java.util.Scanner;
+public class ArithmeticCalculator {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		double Ans;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("eneter the two numbers");
+		int firstnum=sc.nextInt();
+		int secnum= sc.nextInt();
+		System.out.println("Enter the operator we need to perform ");
+		char op=sc.next().charAt(0);
+		double Ans=0;
+
+		switch(op){
+		case '+': Ans=firstnum+secnum;
+		break;
+		case '-':Ans=firstnum-secnum;
+		break;
+		case '*': Ans=firstnum*secnum;
+		break;
+		case '/':Ans=firstnum/secnum;
+		break;
+		}
+		System.out.println("the answer is " +Ans);
+	}
+}
+

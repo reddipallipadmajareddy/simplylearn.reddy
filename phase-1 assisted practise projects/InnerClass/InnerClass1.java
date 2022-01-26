136fba2445526489e056f021c9268888224f782a
reddipallipadmajareddy <97421398+reddipallipadmajareddy@users.noreply.github.com>
2022-01-26 09:42:50 +0530

    Add files via upload
    Initial Commit

---
+++ phase-1 assisted practise projects/InnerClass/InnerClass1.java
@@ -0,0 +1,17 @@
+package com.Lesson2.InnerClass;
+
+public class InnerClass1 {
+	
+		private String message="Hi Ankit"; 
+		class Inner{ 
+		 void hello(){System.out.println(message+", Let us start learning Inner Classes...");} 
+		} 
+		public static void main(String[] args) {
+			InnerClass1 obj=new InnerClass1();
+			InnerClass1.Inner in=obj.new Inner(); 
+		    in.hello(); 
+		}
+	}
+	
+	
+
---
+++ phase-1 assisted practise projects/Thread/ThreadDemo.java
@@ -0,0 +1,14 @@
+package com.Lesson3.Thread;
+
+public class ThreadDemo extends Thread
+{
+	public void run()
+	{
+	 System.out.println("concurrent thread started running..");
+	}
+	public static void main( String args[] )
+	{
+	 ThreadDemo thread = new ThreadDemo();
+	 thread.start();
+    }
+}
